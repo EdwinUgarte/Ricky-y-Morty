cf512f5c8be9f96cb7bb2f3c30b35199196dae3b
EdwinUgarte <Edwin112098>
2022-10-19 17:57:34 -0500

    add build

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,21 +3,23 @@ import { Link, NavLink } from "react-router-dom";
 
 import './css/navbar.css'
 const Navbar = () => {
- 
-        return (
-           
-            <nav className="navbar">
+
+    return (
+
+        <nav >
+            <div className="navbar">
                 <div>
                     <a href="/">
-                     <img src="/images/Rick_and_Morty.svg.png" alt="" />
-                     </a>
-                
+                        <img src="/images/Rick_and_Morty.svg.png" alt="" />
+                    </a>
+
                 </div>
-            
-            </nav>
-          
-        );
-    
+            </div>
+
+        </nav>
+
+    );
+
 }
 
 export default Navbar;
